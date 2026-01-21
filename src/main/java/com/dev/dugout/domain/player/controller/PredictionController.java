2c0d941a7b972d4d506b93c38d3709cb8e01df2e
SungChul23 <kimsam0923@naver.com>
2026-01-21 20:34:54 +0900

    refactor(PredictionController): typo correction

--- src/main/java/com/dev/dugout/domain/player/controller/PredictionController.java
+++ src/main/java/com/dev/dugout/domain/player/controller/PredictionController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/vi")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class PredictionController {
     private final PredictionService analysisService;
