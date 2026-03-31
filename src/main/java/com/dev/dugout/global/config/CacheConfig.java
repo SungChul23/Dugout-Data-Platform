7021d2b582e00921a41746bab1db9ddc5f426ca6
김성철 <143672987+SungChul23@users.noreply.github.com>
2026-04-01 01:14:25 +0900

    feat (Cache) :Caffeine Cache Settings

---
+++ src/main/java/com/dev/dugout/global/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.dev.dugout.global.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+    //스프링 부트 메모리에 결과를 저장할 수 있도록 Caffeine 캐시를 활성화 (선수들 각 지표별 top 5 리더보드에 사용하기 위함)
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                .expireAfterWrite(10, TimeUnit.MINUTES) // 10분 뒤 자동 만료 (DB 부하 제로)
+                .maximumSize(100)); // 최대 100개 항목 유지
+        return cacheManager;
+    }
+}
